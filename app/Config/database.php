84006db347d5db57b7e04cfbc320c68417022805
tienhv <hoangtien.c223@gmail.com>
2015-11-21 14:06:58 +0700

    test connection db

--- app/Config/database.php
+++ app/Config/database.php
@@ -75,7 +75,7 @@ class DATABASE_CONFIG {
         $this->default = array(
             'datasource' => 'Database/Mysql',
             'persistent' => false,
-            'host' => '159.203.197.201',
+            'host' => 'localhost',
             'login' => $_SERVER['DB_USER'],
             'password' => $_SERVER['DB_PASSWORD'],
             'database' => $_SERVER['DB_NAME'],
