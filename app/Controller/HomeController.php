cf5f7ec7ce997e116c582fc1ccec26c9d1e87df8
tienhv <hoangtien.c223@gmail.com>
2015-11-21 11:17:57 +0700

    tach layout

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tienhv
+ * Date: 11/21/15
+ * Time: 11:13 AM
+ */
+App::uses('AppController', 'Controller');
+
+class HomeController extends AppController{
+    public function index(){
+
+    }
+} 
